4949714d7c6e0a6c08ac69d0e31dac36962eaf63
Darren Garnett <garnettd@oregonastate.edu>
2023-01-10 13:57:20 -0800

    Cleaned up and added comments

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -8,34 +8,37 @@ using System.Linq;
 
 public class Command
 {
-    public string line = "test";
-    public string[] args = {"test", "test"};
-    public float time = 0;
+    public string line;
+    public string[] args;
+    public float time;
 }
 
-public class spawning : MonoBehaviour
+public class SceneManager : MonoBehaviour
 {
     private GameObject curObject;
     private string[] commands;
     public string textPath;
     private int index;
-    Command curCommand;
+    private Command curCommand;
 
-    //public Command cmd = new Command();
-
+    //Creates a new instance of the specified prefab with optional specified transformation values
     int spawn(string command, string[] terms)
     {
+        //Assign default command terms
         string prefabName = "default", objectName = "default";
         float xpos = 0, ypos = 0, zpos = 0;
         int xrot = 0, yrot = 0, zrot= 0;
         float xscl = 1, yscl = 1, zscl = 1;
 
+        //Test for minimum terms
         int termCount = terms.Count();
         if(termCount < 4) 
         {
             Debug.Log("Invalid terms for spawning obect with command '" + command + "'");
             return 1;
         }
+
+        //Assign the given terms
         if(termCount >= 4) 
         {
             prefabName = terms[2];
@@ -60,21 +63,24 @@ public class spawning : MonoBehaviour
             zscl = Convert.ToSingle(terms[12]);
         }
         
+        //Create new instance of the given prefab
         curObject = Resources.Load<GameObject>("SimpleTownLite/_Prefabs/" + prefabName);
-
         GameObject NewGameObj = Instantiate(curObject) as GameObject;
-         Quaternion init_rot = Quaternion.Euler(xrot, yrot, zrot);
-        
+
+        //Apply all values to the new GameObject
         NewGameObj.name = objectName;
         NewGameObj.transform.localScale = new Vector3(xscl, yscl, zscl);
+        Quaternion init_rot = Quaternion.Euler(xrot, yrot, zrot);
         NewGameObj.transform.rotation = init_rot;
         NewGameObj.transform.position = new Vector3(xpos, ypos, zpos);
 
         return 0;
     }
 
+    //Translates the specified object by a specified offset
     int translate(string command, string[] terms)
     {
+        //Test for minimum terms
         if(terms.Count() != 6)
         {
             Debug.Log("Invalid terms for moving obect with command '" + command + "'");
@@ -82,6 +88,7 @@ public class spawning : MonoBehaviour
         }
         else
         {
+            //Apply given offset values
             curObject = GameObject.Find(terms[2]);
             float xdis = Convert.ToSingle(terms[3]);
             float ydis = Convert.ToSingle(terms[4]);
@@ -93,8 +100,10 @@ public class spawning : MonoBehaviour
         return 0;
     }
 
+    //Applies the specified animator to the specified object
     int animate(string command, string[] terms)
     {
+        //Test for minimum terms
         if(terms.Count() < 4)
         {
             Debug.Log("Invalid terms for moving obect with command '" + command + "'");
@@ -102,6 +111,7 @@ public class spawning : MonoBehaviour
         }
         else
         {
+            //Apply given animator
             curObject = GameObject.Find(terms[2]);
             Animator curAnimator = curObject.GetComponent<Animator>();
             curAnimator.runtimeAnimatorController = Resources.Load("SimpleTownLite/_Demo/" + terms[3]) as RuntimeAnimatorController;
@@ -111,13 +121,16 @@ public class spawning : MonoBehaviour
         return 0;
     }
 
+    //Determines which command is to be executed
     int execute(string command)
     {
+        //Initialize return value
         int result;
 
         Debug.Log(command);
         string[] terms = command.Split(' ');
-                
+        
+        //Determine command by name
         switch(terms[1])
         {
             case "spawn":
@@ -140,15 +153,11 @@ public class spawning : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Ensure that the input file path is correct
         if(File.Exists(textPath)) 
         {
+            //Store the contents of the input file
             commands = System.IO.File.ReadAllLines(textPath);
-
-            //Debug.Log("File Contents:");
-            foreach(string command in commands)
-            {
-                //int result = execute(command);
-            }
         }
         else Debug.Log("Could not open file");
     }
@@ -156,21 +165,27 @@ public class spawning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Command curCommand = getCommand(index);
+        //Get the current command
         curCommand = getCommand(index);
 
-        //Debug.Log(commands.Count());
-
+        //Ensure the current command's time has been reached, and that the last command has not been reached
         if(Time.time > curCommand.time && index < commands.Count() - 1)
         {
+            //Execute the current command
             execute(curCommand.line);
+
+            //Move to the next command
             index++;
         }
     }
 
+    //Gets the command data for the given index
     Command getCommand(int index)
     {
+        //Create new command object
         Command ret = new Command();
+
+        //Get command data from stored command lines
         ret.line = commands[index];
         ret.args = ret.line.Split(' ');
         ret.time = Convert.ToSingle(ret.args[0]);
