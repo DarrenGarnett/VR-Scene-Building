b8ea2761293b0b883ff5028ee1be1b7208e3c676
Darren Garnett <garnettd@oregonastate.edu>
2023-01-09 21:07:43 -0800

    Commands execute as designated times, plus new commands
    A timestamp for the command is now read from each line in the text file. Assuming the commands are in order within the timeline, they will execute when their designated time is reached. Added translate and animate commands as well. Translate moves the object by the specified offset, and animate applies the specified animator to the object.

--- Assets/Scripts/spawning.cs
+++ Assets/Scripts/spawning.cs
@@ -6,55 +6,66 @@ using System;
 using System.IO;
 using System.Linq;
 
+public class Command
+{
+    public string line = "test";
+    public string[] args = {"test", "test"};
+    public float time = 0;
+}
+
 public class spawning : MonoBehaviour
 {
-    public GameObject curObject;
-    public string[] commands;
+    private GameObject curObject;
+    private string[] commands;
+    public string textPath;
+    private int index;
+    Command curCommand;
 
-    int Spawn(string command, string[] terms)
+    //public Command cmd = new Command();
+
+    int spawn(string command, string[] terms)
     {
-        string objectName = "default";
+        string prefabName = "default", objectName = "default";
         float xpos = 0, ypos = 0, zpos = 0;
         int xrot = 0, yrot = 0, zrot= 0;
         float xscl = 1, yscl = 1, zscl = 1;
 
         int termCount = terms.Count();
-        if(termCount <= 2) 
+        if(termCount < 4) 
         {
             Debug.Log("Invalid terms for spawning obect with command '" + command + "'");
             return 1;
         }
-        if(termCount >= 2) objectName = terms[1];
-        if(termCount >= 5)
+        if(termCount >= 4) 
         {
-            xpos = Convert.ToSingle(terms[2]);
-            ypos = Convert.ToSingle(terms[3]);
-            zpos = Convert.ToSingle(terms[4]);
+            prefabName = terms[2];
+            objectName = terms[3];
         }
-        if(termCount >= 8)
+        if(termCount >= 7)
         {
-            xrot = Convert.ToInt32(terms[5]);
-            yrot = Convert.ToInt32(terms[6]);
-            zrot = Convert.ToInt32(terms[7]);
+            xpos = Convert.ToSingle(terms[4]);
+            ypos = Convert.ToSingle(terms[5]);
+            zpos = Convert.ToSingle(terms[6]);
         }
-        if(termCount >= 11)
+        if(termCount >= 10)
         {
-            xscl = Convert.ToSingle(terms[8]);
-            yscl = Convert.ToSingle(terms[9]);
-            zscl = Convert.ToSingle(terms[10]);
+            xrot = Convert.ToInt32(terms[7]);
+            yrot = Convert.ToInt32(terms[8]);
+            zrot = Convert.ToInt32(terms[9]);
+        }
+        if(termCount >= 13)
+        {
+            xscl = Convert.ToSingle(terms[10]);
+            yscl = Convert.ToSingle(terms[11]);
+            zscl = Convert.ToSingle(terms[12]);
         }
         
-        //string matPath = "SimpleTownLite/_Materials/SimpleTownLite_Dumpster";
-
-        //curObject = Resources.Load<GameObject>("SimpleTownLite/_Prefabs/Vehicles_PizzaCar");
-        curObject = Resources.Load<GameObject>("SimpleTownLite/_Prefabs/" + objectName);
+        curObject = Resources.Load<GameObject>("SimpleTownLite/_Prefabs/" + prefabName);
 
         GameObject NewGameObj = Instantiate(curObject) as GameObject;
-
-        Quaternion init_rot = Quaternion.Euler(xrot, yrot, zrot);
-        //Renderer init_rend = NewGameObj.GetComponent<MeshRenderer>();
-
-        //init_rend.material = Resources.Load<Material>(matPath);
+         Quaternion init_rot = Quaternion.Euler(xrot, yrot, zrot);
+        
+        NewGameObj.name = objectName;
         NewGameObj.transform.localScale = new Vector3(xscl, yscl, zscl);
         NewGameObj.transform.rotation = init_rot;
         NewGameObj.transform.position = new Vector3(xpos, ypos, zpos);
@@ -62,56 +73,108 @@ public class spawning : MonoBehaviour
         return 0;
     }
 
+    int translate(string command, string[] terms)
+    {
+        if(terms.Count() != 6)
+        {
+            Debug.Log("Invalid terms for moving obect with command '" + command + "'");
+            return 1;
+        }
+        else
+        {
+            curObject = GameObject.Find(terms[2]);
+            float xdis = Convert.ToSingle(terms[3]);
+            float ydis = Convert.ToSingle(terms[4]);
+            float zdis = Convert.ToSingle(terms[5]);
+
+            curObject.transform.position += new Vector3(xdis, ydis, zdis);
+        }
+
+        return 0;
+    }
+
+    int animate(string command, string[] terms)
+    {
+        if(terms.Count() < 4)
+        {
+            Debug.Log("Invalid terms for moving obect with command '" + command + "'");
+            return 1;
+        }
+        else
+        {
+            curObject = GameObject.Find(terms[2]);
+            Animator curAnimator = curObject.GetComponent<Animator>();
+            curAnimator.runtimeAnimatorController = Resources.Load("SimpleTownLite/_Demo/" + terms[3]) as RuntimeAnimatorController;
+            curAnimator.SetFloat("speed_multi", Convert.ToSingle(terms[4]));
+        }
+
+        return 0;
+    }
+
+    int execute(string command)
+    {
+        int result;
+
+        Debug.Log(command);
+        string[] terms = command.Split(' ');
+                
+        switch(terms[1])
+        {
+            case "spawn":
+                result = spawn(command, terms);
+                break;
+            case "translate":
+                result = translate(command, terms);
+                break;
+            case "animate":
+                result = animate(command, terms);
+                break;
+            default:
+                Debug.Log("Unrecognized command: '" + terms[1] + "'");
+                result = -1;
+                break;
+        }
+        return result;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        string textPath = @"Assets/Text Files/input.txt";
         if(File.Exists(textPath)) 
         {
             commands = System.IO.File.ReadAllLines(textPath);
-            Debug.Log("File Contents:");
+
+            //Debug.Log("File Contents:");
             foreach(string command in commands)
             {
-                int result;
-
-                Debug.Log(command);
-                string[] terms = command.Split(' ');
-                foreach(string term in terms)
-                {
-                    //Debug.Log("term = " + term);
-                }
-                if(terms[0] == "spawn") result = Spawn(command, terms);
-                else Debug.Log("Unrecognized command: '" + terms[0] + "'");
+                //int result = execute(command);
             }
         }
         else Debug.Log("Could not open file");
-/*
-        //curObject = (GameObject)Resources.Load("SimpleTownLite/_Prefabs/Vehicles_PizzaCar", typeof(GameObject));
-        curObject = Resources.Load<GameObject>("SimpleTownLite/_Prefabs/Vehicles_PizzaCar");
-
-        for(int i = 0; i < 5; i++)
-        {
-            GameObject NewGameObj = Instantiate(curObject) as GameObject;
-
-            Quaternion init_rot = Quaternion.Euler(xrot, yrot, zrot);
-            Renderer init_rend = NewGameObj.GetComponent<MeshRenderer>();
-
-            //init_rend.material = Resources.Load<Material>("SimpleTownLite/_Materials/SimpleTownLite_Dumpster");
-            init_rend.material = Resources.Load<Material>(matPath);
-            NewGameObj.transform.localScale = new Vector3(xscl, yscl, zscl);
-            NewGameObj.transform.rotation = init_rot;
-            NewGameObj.transform.position = new Vector3(xpos + i, ypos + i, zpos + i);
-
-            //curObject = (GameObject)PrefabUtility.InstantiatePrefab(curObject as GameObject);
-            //Instantiate(curObject, transform.position, transform.rotation);
-
-        }
-*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Command curCommand = getCommand(index);
+        curCommand = getCommand(index);
+
+        //Debug.Log(commands.Count());
+
+        if(Time.time > curCommand.time && index < commands.Count() - 1)
+        {
+            execute(curCommand.line);
+            index++;
+        }
+    }
+
+    Command getCommand(int index)
+    {
+        Command ret = new Command();
+        ret.line = commands[index];
+        ret.args = ret.line.Split(' ');
+        ret.time = Convert.ToSingle(ret.args[0]);
+
+        return ret;
     }
 }
