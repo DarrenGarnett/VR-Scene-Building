dbd5588e867fb4004573c68d6bf8263f6da1f75b
Darren Garnett <91292897+DarrenGarnett@users.noreply.github.com>
2023-06-27 13:23:22 -0700

    Creates/Updates prefabs.txt with prefab names

--- Assets/Scripts/FileManager.cs
+++ Assets/Scripts/FileManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEditor;
 using TMPro;
 using System.IO;
 using System;
@@ -11,25 +12,29 @@ public class FileManager : MonoBehaviour
 {
     public static string textPath = Application.streamingAssetsPath + "/Text Files/";
     public static List<string> textFiles = new List<string>();
+    private string rootPath;
+    private bool inEditor;
 
     void OnEnable()
     {
         MoveTextFiles();
+        PefabFilesPlaceholder();
     }
 
     void MoveTextFiles()
     {
         int dirCount = Directory.GetDirectories(Application.streamingAssetsPath).Length;
         //Debug.LogError("Dirs in streaming assets?: " + dirCount);
+        inEditor = textPath.Contains("/Assets/");
 
         string[] path = textPath.Split('/');
-        string rootPath = "";
+        rootPath = "";
         for(int i = 0; i < path.Length - 4; i++) rootPath += "/" + path[i];
         rootPath = rootPath.Remove(0, 1);
 
         textPath = rootPath + "/Text Files/";
 
-        if(!textPath.Contains("/Assets/") && dirCount > 0)
+        if(!inEditor && dirCount > 0)
         {            
             //https://stackoverflow.com/questions/70694442/directory-move-throws-ioexception-regardless-of-the-fileshare-options-for-inte
             string source = Application.streamingAssetsPath;
@@ -61,5 +66,16 @@ public class FileManager : MonoBehaviour
                 textFiles.Add(files[i].Remove(0, textPath.Length));
             }
         }
+    }
+
+    //https://stackoverflow.com/questions/53968958/how-can-i-get-all-prefabs-from-a-assets-folder-getting-not-valid-cast-exception
+    void PefabFilesPlaceholder()
+    {
+        GameObject[] prefabs = Resources.LoadAll<GameObject>("Prefabs");
+
+        List<string> prefabNames = new List<string>();
+        foreach(GameObject prefab in prefabs) prefabNames.Add(prefab.name);
+
+        if(!inEditor) File.WriteAllLines(rootPath + "/prefabs.txt", prefabNames);
     }
 }
